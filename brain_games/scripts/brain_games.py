6a9079a9e9f0efca6cc194697a09fc305bfbde1f
Анисии <117117637+anisimV@users.noreply.github.com>
2025-10-20 22:55:03 +0300

    new version

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,7 +1,9 @@
 # brain_games/scripts/brain_games.py
+from brain_games.cli import welcome_user
 
 def main():
     print("Welcome to the Brain Games!")
+    welcome_user()
 
 
 # Точка входа — вызов main() только при запуске модуля как программы
